ab002c4985ae72af26d3bc119f78b0eb6a594fa8
Sam Cronin <croninsw@gmail.com>
2019-07-10 16:49:35 -0500

    Small changes to key inputs and showing a score.

--- snake.py
+++ snake.py
@@ -19,15 +19,22 @@ snake = [
 food = [screenheight/2, screenwidth/2]
 window.addch(int(food[0]), int(food[1]), curses.ACS_DIAMOND)
 
-key = curses.KEY_DOWN
+key = curses.KEY_RIGHT
+score = 0
 
-while True:
+while key != 27:
    next_key = window.getch()
+   prev_key = key
+   if key not in [curses.KEY_LEFT, curses.KEY_RIGHT, curses.KEY_UP, curses.KEY_DOWN, 27]:
+       key = prev_key
    key = key if next_key == -1 else next_key
 
-   if snake[0][0] in [0, screenheight] or snake[0][1] in [0, screenwidth] or snake[0] in snake[1:]:
-       curses.endwin()
-       quit()
+   if snake[0][0] in [0, screenheight] or snake[0][1] in [0, screenwidth] or snake[0] in snake[1:]: break
+    #    curses.endwin()
+
+    #    quit()         no longer needed if the escape key is how the user exits the game
+
+
 
    new_head = [snake[0][0], snake[0][1]]
 
@@ -44,6 +51,7 @@ while True:
 
    if snake[0] == food:
        food = None
+       score += 1
        while food is None:
            newfood = [
                random.randint(1, screenheight-1),
@@ -56,4 +64,7 @@ while True:
        window.addch(int(tail[0]), int(tail[1]), ' ')
 
    window.addch(int(snake[0][0]), int(snake[0][1]), curses.ACS_CKBOARD)
+
+curses.endwin()
+print("\nScore -- " + str(score))
 
